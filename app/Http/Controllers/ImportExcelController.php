176e4e7b2dc2432928e92fbeec8f2e3afa03beaf
miladbarazandeh <miladbrznd@gmail.com>
2019-07-25 15:46:08 +0430

    auth

--- app/Http/Controllers/ImportExcelController.php
+++ app/Http/Controllers/ImportExcelController.php
@@ -3,17 +3,30 @@
 namespace App\Http\Controllers;
 
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Request;
 
 class ImportExcelController extends Controller
 {
     public function importUsersAction(Request $request)
     {
-        $file_n = storage_path('data.csv');
-        $csv = array_map("str_getcsv", file($file_n));
-        $keys = array_shift($csv);
-        foreach ($csv as $i=>$row) {
-            $csv[$i] = array_combine($keys, $row);
+        try {
+            $file_n = storage_path('data.csv');
+            $csv = array_map("str_getcsv", file($file_n));
+            $keys = array_shift($csv);
+            foreach ($csv as $i=>$row) {
+                $csv[$i] = array_combine($keys, $row);
+            }
+
+            foreach ($csv as &$item) {
+                $password = $item['password'];
+                $item['password']= Hash::make($password);
+            }
+
+            User::insert($csv);
+        } catch (\Exception $exception) {
+            return response()->json(['error'=>$exception->getMessage()], 400);
         }
         return $csv;
     }
