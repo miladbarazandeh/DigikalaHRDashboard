2222db604cbc9af723cae4ca99781433120e340b
miladbarazandeh <miladbrznd@gmail.com>
2019-08-04 08:03:37 +0430

    users

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsersController extends Controller
+{
+    public function getAllUsers(Request $request)
+    {
+        try {
+            $users = User::all(['id', 'name']);
+
+            return response()->json($users, 200);
+        } catch (\Exception $exception) {
+            return response()->json($exception->getMessage(), 400);
+        }
+    }
+}
+
