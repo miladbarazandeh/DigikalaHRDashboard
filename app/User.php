fd8c0b321dcd8b8bb0b7c6ea92c085a18f20fbda
miladbarazandeh <miladbrznd@gmail.com>
2019-08-18 13:49:11 +0430

    fix

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'role', 'form_id', 'assigned_user_ids',
+        'name', 'email', 'role', 'form_id', 'assigned_user_ids', 'password',
     ];
 
     /**
