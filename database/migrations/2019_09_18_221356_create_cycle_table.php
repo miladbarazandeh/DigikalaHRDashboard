46034c8488f698bb0756da76946f798f60cc8831
miladbarazandeh <miladbrznd@gmail.com>
2019-09-19 17:16:10 +0430

    fix

--- database/migrations/2019_09_18_221356_create_cycle_table.php
+++ database/migrations/2019_09_18_221356_create_cycle_table.php
@@ -15,6 +15,7 @@ class CreateCycleTable extends Migration
     {
         Schema::create('cycle', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
             $table->boolean('active');
             $table->timestamps();
         });
