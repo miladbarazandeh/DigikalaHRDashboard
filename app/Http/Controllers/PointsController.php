8c4d38e3eb1296d9f758b2985db80fb235cd1669
miladbarazandeh <miladbrznd@gmail.com>
2019-10-03 17:48:00 +0330

    fix

--- app/Http/Controllers/PointsController.php
+++ app/Http/Controllers/PointsController.php
@@ -178,6 +178,8 @@ public function setPointAction(Request $request)
                 throw new \Exception('Relation Not found');
             }
 
+            return $relation;
+
             $pointEntity = Points::where('relation_id', $relation->id)->where('parameter_id', $parameterId);
             if(!$pointEntity->get()) {
                 $pointDB = new Points(
