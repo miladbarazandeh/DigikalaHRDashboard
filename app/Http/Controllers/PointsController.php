00e2a97356c84236a17b63789735b83a94cacba0
miladbarazandeh <miladbrznd@gmail.com>
2019-10-03 18:02:08 +0330

    fix

--- app/Http/Controllers/PointsController.php
+++ app/Http/Controllers/PointsController.php
@@ -184,7 +184,7 @@ public function setPointAction(Request $request)
 
             $pointEntity = Points::where('relation_id', $relation->id)->where('parameter_id', $parameterId)->first();
 
-            if(empty($pointEntity)) {
+            if(!$pointEntity) {
                 $pointDB = new Points(
                     [
                         'parameter_id'=>$parameterId,
