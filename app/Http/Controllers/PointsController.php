1432d39097c9173841b7978601d35ddae7dda2ba
miladbarazandeh <miladbrznd@gmail.com>
2019-10-03 16:42:29 +0330

    fix

--- app/Http/Controllers/PointsController.php
+++ app/Http/Controllers/PointsController.php
@@ -220,7 +220,7 @@ public function setPointAction(Request $request)
             $totalPoint = 0;
             foreach ($parameters as $parameter) {
                 $point = $pointsEntity->where('parameter_id', $parameter['id'])->first();
-                $parameterPoint = $point * $parameter['weight'];
+                $parameterPoint = $point->point * $parameter['weight'];
                 $categoryId = $parameter['categoryId'];
                 foreach ($categories as $category) {
                     if ($category['id'] == $categoryId) {
