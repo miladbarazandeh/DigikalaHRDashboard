46e94d51c2e3d00df09c8a8e4ff171bb1e5f5898
miladbarazandeh <miladbrznd@gmail.com>
2020-02-17 19:43:31 +0330

    fix

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -24,9 +24,10 @@ class CardsController extends Controller
             $url = $request->get('url');
             $id = $request->get('card_id');
             $show = $request->get('show');
-            $file = $request->file('file');
 
-            if ($file != 'null') {
+
+            if ($request->has('file')) {
+                $file = $request->file('file');
                 $extension = $file->getClientOriginalExtension();
                 $fileName = time().'.'.$extension;
                 $file->move($baseUrl, $fileName);
